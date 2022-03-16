5260c76735e99541774130b2ab11466dd905a1b8
DARLINGTON-CG <yeboahdarlingtonk@gmail.com>
2022-03-16 23:52:26 +0000

    signout and user state added

---
+++ lib/application/auth/auth_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodshare/domain/auth/i_auth_facade.dart';
+import 'package:foodshare/domain/auth/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+part 'auth_bloc.freezed.dart';
+
+@injectable
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final IAuthFacade _authFacade;
+
+  AuthBloc(this._authFacade) : super(const AuthState.initial()) {
+    on<AuthCheckRequested>(_onAuthCheckStatusRequested);
+    on<SignedOut>(_onSignedOut);
+  }
+
+  void _onAuthCheckStatusRequested(
+      AuthCheckRequested event, Emitter<AuthState> emit) async {
+    final Option<LocalUser> userOption = _authFacade.getSignedInUser();
+    emit(userOption.fold(() => const AuthState.unauthenticated(),
+        (_) => const AuthState.authenticated()));
+  }
+
+  void _onSignedOut(SignedOut event, Emitter<AuthState> emit) async {
+    await _authFacade.signOut();
+    emit(const AuthState.unauthenticated());
+  }
+
+  @override
+  AuthState get initialState => const AuthState.initial();
+}
--- lib/injector.config.dart
+++ lib/injector.config.dart
@@ -8,11 +8,12 @@ import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import 'application/auth/auth_bloc.dart' as _i7;
 import 'application/auth/sign_in_form/sign_in_form_bloc.dart' as _i6;
 import 'domain/auth/i_auth_facade.dart' as _i4;
 import 'infrastructure/auth/firebase_auth_facade.dart' as _i5;
 import 'infrastructure/core/firebase_injectable_module.dart'
-    as _i7; // ignore_for_file: unnecessary_lambdas
+    as _i8; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -26,7 +27,8 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
       () => _i5.FirebaseAuthFacade(get<_i3.FirebaseAuth>()));
   gh.factory<_i6.SignInFormBloc>(
       () => _i6.SignInFormBloc(get<_i4.IAuthFacade>()));
+  gh.factory<_i7.AuthBloc>(() => _i7.AuthBloc(get<_i4.IAuthFacade>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i7.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i8.FirebaseInjectableModule {}
