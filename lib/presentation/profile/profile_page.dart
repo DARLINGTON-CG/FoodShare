066d189a7bf49e617c6d24ef4547c5ad122a3e78
DARLINGTON-CG <48848489+DARLINGTON-CG@users.noreply.github.com>
2022-04-09 15:21:00 +0000

    profile page design complete

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -4,7 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../anim/page/slide_in.dart';
 
-
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -15,7 +14,7 @@ class ProfilePage extends StatelessWidget {
         slivers: <Widget>[
           SliverAppBar(
             title: Text('Profile',
-                style: GoogleFonts.lato(fontSize: 18, color: Colors.black)),
+                style: GoogleFonts.lato(fontSize: 17, color: Colors.black)),
             centerTitle: true,
             elevation: 0.0,
             leading: IconButton(
@@ -27,102 +26,326 @@ class ProfilePage extends StatelessWidget {
             pinned: true,
           ),
           SliverToBoxAdapter(
-            child: SizedBox(
+            child: Container(
               width: MediaQuery.of(context).size.width,
-              height: 100,
-              child: const Center(
-                child: CircleAvatar(backgroundColor: Colors.black38, radius: 40),
+              height: 20,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
               ),
             ),
           ),
-          SliverFillRemaining(
-            child: Column(children: <Widget>[
-              ListTile(
-                  leading: Container(
-                    width: 55,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xFF119DA4)),
-                    child: const Center(
-                        child:
-                            Icon(Icons.data_usage_sharp, color: Colors.black)),
-                  ),
-                  title: Text(
-                    'Data',
-                    style: GoogleFonts.lato(
-                      fontSize: 17,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  subtitle: Text('Personal user data',
-                      style: GoogleFonts.lato(
-                          fontSize: 15))),
-              ListTile(
-                  leading: Container(
-                    width: 55,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xFFFFC857)),
-                    child: const Center(
-                        child: Icon(Icons.face, color: Colors.black)),
-                  ),
-                  title: Text(
-                    'Friends',
-                    style: GoogleFonts.lato(
-                      fontSize: 17,
-                      color: Colors.black,fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Text('Friendly system users',
-                      style: GoogleFonts.lato(
-                          fontSize: 15))),
-              ListTile(
-                  leading: Container(
-                    width: 55,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xFF98F5E1)),
-                    child: const Center(
-                        child: Icon(Icons.phone_in_talk_rounded,
-                            color: Colors.black)),
-                  ),
-                  title: Text(
-                    'Chats',
-                    style: GoogleFonts.lato(
-                      fontSize: 17,
-                      color: Colors.black,fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Text('Friendly system users',
-                      style: GoogleFonts.lato(
-                          fontSize: 15, color: Colors.grey))),
-              ListTile(
-                  leading: Container(
-                    width: 55,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color(0xFF89FC00)),
-                    child: const Center(
-                        child: Icon(Icons.settings, color: Colors.black)),
-                  ),
-                   onTap: () => Navigator.of(context).push(SlideIn(page: const AccountSettingsPage())),
-                  title: Text(
-                    'Account Settings',
-                    style: GoogleFonts.lato(
-                      fontSize: 17,
-                      color: Colors.black,fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  subtitle: Text('System updates and alerts',
-                      style: GoogleFonts.lato(
-                          fontSize: 15, color: Colors.grey))),
-            ]),
-          )
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: ListTile(
+                title: Text(
+                  "Payment Configuration",
+                  style: GoogleFonts.lato(fontSize: 14, color: const Color(0xFF3212F1)),
+                ),
+              ),
+            ),
+          ),
+        
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+              ),
+              child: ListTile(
+                title: Text(
+                  "App Configuration",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Information",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Push Notifications",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Dark Mode",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+                trailing: Text(
+                  "Off",
+                  style: GoogleFonts.lato(fontSize: 14, color: const Color(0xFF3212F1)),
+                ),
+              
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Delete Timer",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+        
+        
+        
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+              ),
+              child: ListTile(
+                title: Text(
+                  "Account Configuration",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Delete Account",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Sign out",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+            
+        
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+              ),
+              child: ListTile(
+                title: Text(
+                  "About",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Help",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color:  Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.withOpacity(0.1))
+                )
+              ),
+              child: ListTile(
+                title: Text(
+                  "Privacy policy",
+                  style: GoogleFonts.lato(fontSize: 14, color: Colors.black),
+                ),
+              
+              ),
+            ),
+          ),
+            
+          // SliverFillRemaining(
+          //   child: Column(children: <Widget>[
+          //     ListTile(
+          //         leading: Container(
+          //           width: 55,
+          //           height: 80,
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(20),
+          //               color: const Color(0xFF119DA4)),
+          //           child: const Center(
+          //               child:
+          //                   Icon(Icons.data_usage_sharp, color: Colors.black)),
+          //         ),
+          //         title: Text(
+          //           'Data',
+          //           style: GoogleFonts.lato(
+          //               fontSize: 17,
+          //               color: Colors.black,
+          //               fontWeight: FontWeight.bold),
+          //         ),
+          //         subtitle: Text('Personal user data',
+          //             style: GoogleFonts.lato(fontSize: 15))),
+          //     ListTile(
+          //         leading: Container(
+          //           width: 55,
+          //           height: 80,
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(20),
+          //               color: const Color(0xFFFFC857)),
+          //           child: const Center(
+          //               child: Icon(Icons.face, color: Colors.black)),
+          //         ),
+          //         title: Text(
+          //           'Friends',
+          //           style: GoogleFonts.lato(
+          //             fontSize: 17,
+          //             color: Colors.black,
+          //             fontWeight: FontWeight.bold,
+          //           ),
+          //         ),
+          //         subtitle: Text('Friendly system users',
+          //             style: GoogleFonts.lato(fontSize: 15))),
+          //     ListTile(
+          //         leading: Container(
+          //           width: 55,
+          //           height: 80,
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(20),
+          //               color: const Color(0xFF98F5E1)),
+          //           child: const Center(
+          //               child: Icon(Icons.phone_in_talk_rounded,
+          //                   color: Colors.black)),
+          //         ),
+          //         title: Text(
+          //           'Chats',
+          //           style: GoogleFonts.lato(
+          //             fontSize: 17,
+          //             color: Colors.black,
+          //             fontWeight: FontWeight.bold,
+          //           ),
+          //         ),
+          //         subtitle: Text('Friendly system users',
+          //             style:
+          //                 GoogleFonts.lato(fontSize: 15, color: Colors.grey))),
+          //     ListTile(
+          //         leading: Container(
+          //           width: 55,
+          //           height: 80,
+          //           decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(20),
+          //               color: const Color(0xFF89FC00)),
+          //           child: const Center(
+          //               child: Icon(Icons.settings, color: Colors.black)),
+          //         ),
+          //         onTap: () => Navigator.of(context)
+          //             .push(SlideIn(page: const AccountSettingsPage())),
+          //         title: Text(
+          //           'Account Settings',
+          //           style: GoogleFonts.lato(
+          //             fontSize: 17,
+          //             color: Colors.black,
+          //             fontWeight: FontWeight.bold,
+          //           ),
+          //         ),
+          //         subtitle: Text('System updates and alerts',
+          //             style:
+          //                 GoogleFonts.lato(fontSize: 15, color: Colors.grey))),
+          //   ]),
+          // )
         ],
       ),
     );
